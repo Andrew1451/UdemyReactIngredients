35fb4d0c9178e02e9c331ee2350d012c952ded9a
Andrew <andrew.gaynor@ymail.com>
2020-01-13 12:27:08 -0500

    retrieve ingredients list via http-request/database

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,9 +6,35 @@ import Search from './Search';
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch('https://react-hooks-26b43.firebaseio.com/ingredients.json')
+      .then(res => res.json())
+      .then(data => {
+        const loadedIngredients = [];
+        for (const key in data) {
+          loadedIngredients.push({
+            id: key,
+            title: data[key].title,
+            amount: data[key].amount
+          })
+        }
+        setUserIngredients(loadedIngredients);
+      })
+  }, []);
+
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}]);
-  }
+    fetch('https://react-hooks-26b43.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      return res.json();
+    }).then(data => {
+      setUserIngredients(prevIngredients => [...prevIngredients, {id: data.name, ...ingredient}]);
+    });
+  };
 
   const removeIngredientHandler = id => {
     setUserIngredients(prevIngredients => prevIngredients.filter(ing => ing.id !== id))
